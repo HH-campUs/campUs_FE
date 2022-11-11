5edaba2fc8e560b3fd2c449358a393159cf3f56c
buddle6091 <buddle6091@naver.com>
2022-11-11 13:53:35 +0900

    Initial setting #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+/* Switch가 react-router-dom ver 6 넘어가며 Switch를 지원 안하게 됨 -> Routes */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Mypage from "./pages/Mypage";
+import Result from "./pages/Result";
+import Error from "./pages/Error";
+import NotFound from "./pages/NotFound";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* 혹시모를 치명적인 Error를 서비스 이용자가 보지 못하게 에러 페이지로 보내버림 */
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="result" element={<Result />} />
+
+          <Route path="detail" element={<Detail />} />
+          <Route path="detail/:id" element={<Detail />} />
+          <Route path="mypage" element={<Mypage />} />
+          <Route path="mypage/:id" element={<Mypage />} />
+
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 

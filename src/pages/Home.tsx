2b99f3b3638f40da0337cd0f6c01d9b8122affd7
buddle6091 <buddle6091@naver.com>
2022-11-11 14:19:46 +0900

    api 호출 예시

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import Carousel from "../components/Carousel";
 
 function Home() {
+  useEffect(() => {
+    async function a() {
+      const res = await axios.get(
+        "https://apis.data.go.kr/B551011/GoCamping/basedList?MobileOS=ETC&MobileApp=gocamp&serviceKey=hxdpEFH4tR8PQCTY8sfFyV78s69uVxnI1z7AJkIgBlkk2kHb0WxnFqZjaLQ9sbfo%2B5C7c47t74J1aNmioNNrwg%3D%3D&_type=json"
+      );
+      console.log(res);
+    }
+    a();
+  }, []);
+
+  useEffect(() => {
+    async function b() {
+      const r = await axios.get(
+        "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=eixV3mOlRoqUyi%2BK9P6%2BS3BQMBXCroFM31lGc%2BOSp80JFNv7B8%2FiCEV49OSfF2bchwh5N7z50Sw8OQFWCkZbDg%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20221108&base_time=0500&nx=55&ny=127"
+      );
+      console.log(r);
+    }
+    b();
+  }, []);
+
   return (
     <div>
       Home

a52d65a04f722be472d0cad60e6a0932dc020e11
buddle6091 <buddle6091@naver.com>
2022-11-21 22:30:14 +0900

    branch 오류 해결

--- src/interfaces/get.ts
+++ src/interfaces/get.ts
@@ -8,3 +8,32 @@ export interface IGetCampReview {
   reviewImg: string;
   reviewComment: string;
 }
+
+export interface IGetCampResult {
+  campId: number;
+  campName: string;
+  /* 야영장 종류 */
+  induty: string;
+  doNm: string;
+  /*  */
+  sigunguNm: string;
+  address: string;
+  X: string | number;
+  Y: string | number;
+  oerPdcl: string;
+  operDecl: string;
+  animal: string;
+  ImageUrl: string;
+  homepage: string;
+  sbrsCl: string;
+  posblFcltyCl: string;
+  wtrplCo: string;
+  swrmCo: string;
+  toiletCo: string;
+  manageSttus: string;
+  themaEnvrnCl: string;
+  lookup: string;
+  createdtime: string;
+  eqpmnLendCl: string;
+  reviewCount: number;
+}

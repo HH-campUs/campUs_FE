3fc3b7919d8fc35c5101a3ce59bcd184f68f6994
buddle6091 <buddle6091@naver.com>
2022-12-12 16:57:52 +0900

    delete unused library

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 /* import { ReactQueryDevtools } from "react-query/devtools"; */
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { toast, ToastContainer } from "react-toastify";
 /* import "./style/transition.css"; */
 
 import Home from "../pages/Home";

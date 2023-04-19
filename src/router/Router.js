0f4ae47f1a7699ab78789dcabc23c648eaeb8a3a
buddle6091 <buddle6091@naver.com>
2023-04-19 16:03:24 +0900

    [fixed] webpack env error clear

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,21 +1,22 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import Home from "../pages/Home";
-import Login from "../pages/Login";
+/* import Login from "../pages/Login"; */
 import SignUp from "../pages/SignUp";
 import Welcome from "../pages/Welcome";
 import Splash from "../pages/Splash";
 import KaKaoAuth from "../pages/OAuth/KaKaoAuth";
 import GoogleAuth from "../pages/OAuth/GoogleAuth";
 
-import Topic from "../pages/Topic";
+/* import Topic from "../pages/Topic"; */
 import Detail from "../pages/Detail";
 import Ddetail from "../pages/DetailPage/Ddetail";
 import Dreview from "../pages/DetailPage/Dreview";
-import Result from "../pages/Result";
+/* import Result from "../pages/Result";
 import Keyword from "../pages/Keyword";
-import Mypage from "../pages/Mypage";
+import Mypage from "../pages/Mypage"; */
 
 import NotFound from "../pages/NotFound";
 import Layout from "../layout/Layout";
@@ -27,38 +28,82 @@ import Review from "../pages/Review";
 /* Switch가 react-router-dom ver 6 넘어가며 Switch를 지원 안하게 됨 -> Routes */
 
 function Router() {
+  /* Lazy를 활용하여 페이지 분리하기 */
+  const Home = lazy(() =>
+    /* webpackChunkName: "Home" */ import("../pages/Home")
+  );
+  const Result = lazy(() =>
+    /* webpackChunkName: "Result" */ import("../pages/Result")
+  );
+  const Keyword = lazy(() =>
+    /* webpackChunkName: "Keyword" */ import("../pages/Keyword")
+  );
+  const Topic = lazy(() =>
+    /* webpackChunkName: "Topic" */ import("../pages/Topic")
+  );
+  const Login = lazy(() =>
+    /* webpackChunkName: "Login" */ import("../pages/Login")
+  );
+  const SignUp = lazy(() =>
+    /* webpackChunkName: "SignUp" */ import("../pages/SignUp")
+  );
+  const Welcome = lazy(() =>
+    /* webpackChunkName: "Welcome" */ import("../pages/Welcome")
+  );
+
+  const Mypage = lazy(() =>
+    /* webpackChunkName: "Mypage" */ import("../pages/Mypage")
+  );
+  const NotFound = lazy(() =>
+    /* webpackChunkName: "NotFound" */ import("../pages/NotFound")
+  );
+  const MyReview = lazy(() =>
+    /* webpackChunkName: "MyReview" */ import("../pages/Mypage/MyReview")
+  );
+  const MyPick = lazy(() =>
+    /* webpackChunkName: "MyPick" */ import("../pages/Mypage/MyPick")
+  );
+  const MyPlan = lazy(() =>
+    /* webpackChunkName: "MyPlan" */ import("../pages/Mypage/MyPlan")
+  );
+  const Detail = lazy(() =>
+    /* webpackChunkName: "Detail" */ import("../pages/Detail")
+  );
+
   return (
     <BrowserRouter>
       <Splash />
-      <Layout>
-        <TransitionGroup>
-          <CSSTransition timeout={500}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="result" element={<Result />} />
-              <Route path="keyword" element={<Keyword />} />
-              <Route path="topic/:topicId" element={<Topic />} />
-              <Route path="login" element={<Login />} />
-              <Route path="/kakao/callback" element={<KaKaoAuth />} />
-              <Route path="/google/callback" element={<GoogleAuth />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route path="Welcome" element={<Welcome />} />
-              <Route path="/detail/:campId" element={<Detail />}>
-                <Route path="/detail/:campId/detail" element={<Ddetail />} />
-                <Route path="/detail/:campId/review" element={<Dreview />} />
-              </Route>
-              <Route path="/review/:campId" element={<Review />} />
-              <Route path="mypage" element={<Mypage />} />
-              <Route path="/mypage/" element={<Mypage />}>
-                <Route path="/mypage/myreview" element={<MyReview />} />
-                <Route path="/mypage/mypick" element={<MyPick />} />
-                <Route path="/mypage/myplan" element={<MyPlan />} />
-              </Route>
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </CSSTransition>
-        </TransitionGroup>
-      </Layout>
+      <Suspense fallback={<div>...loading</div>}>
+        <Layout>
+          <TransitionGroup>
+            <CSSTransition timeout={500}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="result" element={<Result />} />
+                <Route path="keyword" element={<Keyword />} />
+                <Route path="topic/:topicId" element={<Topic />} />
+                <Route path="login" element={<Login />} />
+                <Route path="/kakao/callback" element={<KaKaoAuth />} />
+                <Route path="/google/callback" element={<GoogleAuth />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="Welcome" element={<Welcome />} />
+                <Route path="/detail/:campId" element={<Detail />}>
+                  <Route path="/detail/:campId/detail" element={<Ddetail />} />
+                  <Route path="/detail/:campId/review" element={<Dreview />} />
+                </Route>
+                <Route path="/review/:campId" element={<Review />} />
+                <Route path="mypage" element={<Mypage />} />
+                <Route path="/mypage/" element={<Mypage />}>
+                  <Route path="/mypage/myreview" element={<MyReview />} />
+                  <Route path="/mypage/mypick" element={<MyPick />} />
+                  <Route path="/mypage/myplan" element={<MyPlan />} />
+                </Route>
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
+        </Layout>
+      </Suspense>
     </BrowserRouter>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,50 @@
-//const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const dotenv = require("dotenv");
+
+// load env file
+dotenv.config();
+
+const env = {
+  "process.env.REACT_APP_API": JSON.stringify(process.env.REACT_APP_API),
+  "process.env.REACT_APP_CLIENT_ID": JSON.stringify(
+    process.env.REACT_APP_CLIENT_ID
+  ),
+  "process.env.REACT_APP_REDIRECT_URI": JSON.stringify(
+    process.env.REACT_APP_REDIRECT_URI
+  ),
+  "process.env.REACT_APP_GOGGLE_ID": JSON.stringify(
+    process.env.REACT_APP_GOGGLE_ID
+  ),
+  "process.env.REACT_APP_GOOGLE_SECRET_KEY": JSON.stringify(
+    process.env.REACT_APP_GOOGLE_SECRET_KEY
+  ),
+  "process.env.REACT_APP_GOOGLE_CLIENT_ID": JSON.stringify(
+    process.env.REACT_APP_GOOGLE_CLIENT_ID
+  ),
+  "process.env.REACT_APP_GOOGLE_REDIRECT_URI": JSON.stringify(
+    process.env.REACT_APP_GOOGLE_REDIRECT_URI
+  ),
+};
 
 module.exports = {
-  mode: "production", // 배포할 때에는 "production" master 브랜치에만 적용하면 될 듯
-  entry: "./src/index.tsx",
+  mode: "development", // 배포할 때에는 "production" master 브랜치에만 적용하면 될 듯
   devtool: "inline-source-map",
+  target: ["web", "es5"],
+  entry: "./src/index.tsx",
+  output: {
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].chunk.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    // 새로운 결과물로 디렉토리 엎
+    clean: true,
+  },
+
   module: {
     rules: [
       {
@@ -26,13 +63,17 @@ module.exports = {
       },
     ],
   },
+
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설
   plugins: [
     new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new webpack.DefinePlugin(env),
   ],
+
   devServer: {
     static: path.resolve(__dirname, "dist"),
     compress: true,
@@ -41,17 +82,30 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.join(__dirname, "dist"),
-    clean: true,
-  },
+
   optimization: {
     minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.(ts|js|tsx)$/,
+      }),
+    ],
     runtimeChunk: "single",
     moduleIds: "deterministic",
     splitChunks: {
       chunks: "all",
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
   },
 };
